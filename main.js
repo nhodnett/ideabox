58d5b2cafd63608f1d8ed0e4817984f07895d187
Rory Magee <roryemagee@gmail.com>
2022-01-05 16:01:08 -0700

    Complete card save and insert functions

--- main.js
+++ main.js
@@ -3,16 +3,26 @@ var ideas = [];
 var saveButton = document.querySelector('.save-button');
 var ideaTitle = document.querySelector('#title');
 var ideaBody = document.querySelector('#body');
+var ideaCardGrid = document.querySelector('.idea-card-grid');
 
-saveButton.addEventListener('click', saveIdeaCard)
+saveButton.addEventListener('click', saveIdeaCard);
 
-saveIdeaCard() {
+function saveIdeaCard() {
   var idea = new Idea(ideaTitle.value, ideaBody.value);
   ideas.push(idea);
-  console.log(ideas);
-  // insertIdeaCard();
+  insertIdeaCard();
 }
 
-// function insertIdeaCard() {
-//
-// };
+function insertIdeaCard() {
+  ideaCardGrid.innerHTML += `
+    <div class="idea-card">
+      <div class="user-idea-header"></div>
+      <div class="user-idea">
+        <h3>${ideas[ideas.length-1].title}</h3>
+        <p>${ideas[ideas.length-1].body}</p>
+      </div>
+      <button class="comment-button">Comment</button>
+      <label class="comment hidden"></label>
+    </div>
+  `;
+};
