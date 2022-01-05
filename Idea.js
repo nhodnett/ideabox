7881c53dd227778018870c0f6731f2b1444e539e
Rory Magee <roryemagee@gmail.com>
2022-01-04 20:34:19 -0700

    Add preliminary save button function to DOM

--- Idea.js
+++ Idea.js
@@ -1,13 +1,21 @@
 class Idea {
-  constructor() {
+  constructor(title, body) {
     this.id = Date.now();
-    this.title = "";
-    this.body = "";
+    this.title = title;
+    this.body = body;
     this.star = false;
   }
 
-  updateIdea() {
+  saveIdeaCard() {
+    var idea = new Idea(ideaTitle.value, ideaBody.value);
+    ideas.push(idea);
+    console.log(ideas);
+    //insertIdeaCard();
 
+  }
+
+  updateIdea() {
+    // should update the idea's starred state.
   }
 
 }
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 var ideas = [];
+
+var saveButton = document.querySelector('.save-button');
+var ideaTitle = document.querySelector('#title');
+var ideaBody = document.querySelector('#body');
+
+saveButton.addEventListener('click', saveIdeaCard)
+
+saveIdeaCard() {
+  var idea = new Idea(ideaTitle.value, ideaBody.value);
+  ideas.push(idea);
+  console.log(ideas);
+  // insertIdeaCard();
+}
+
+// function insertIdeaCard() {
+//
+// };
