c97fce29fede6dda50240408a61d7e7378e48c5a
Rory Magee <roryemagee@gmail.com>
2022-01-09 12:08:16 -0700

    Fix show starred ideas toggle

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var ideas = [];
-var filterIdeas = true;
+var filterIdeasStatus = false;
+var searchIdeasStatus = false;
 var inputData;
-var inputString;
 var searchIndex = [];
 
 var saveButton = document.querySelector('.save-button');
@@ -19,26 +19,21 @@ saveButton.addEventListener('mouseout', enableSaveButton);
 ideaSection.addEventListener('click', deleteIdeaCard);
 ideaSection.addEventListener('click', changeImage);
 borderBox.addEventListener('click', showStarredIdeas);
-borderBox.addEventListener('click', toggleFilterButton);
+// borderBox.addEventListener('click', toggleFilterButton);
 borderBox.addEventListener('click', showAllIdeas);
-searchBox.addEventListener('input', outputSearch);
+// searchBox.addEventListener('input', outputSearch);
 
 function saveIdeaCard() {
   if (ideaTitle.value != false && ideaBody.value != false) {
-  var idea = new Idea(ideaTitle.value, ideaBody.value);
-  ideas.push(idea);
-  regenerateIdeaCards();
-  ideaTitle.value = "";
-  ideaBody.value = "";
+    var idea = new Idea(ideaTitle.value, ideaBody.value);
+    ideas.push(idea);
+  if (searchIdeasStatus === false) {
+    regenerateIdeaCards(ideas);
+  } else {
+    regenerateIdeaCards(searchIndex);
   }
-}
-
-function disableSaveButton(event) {
-  if (event.target.className === 'save-button') {
-    if (ideaTitle.value == false || ideaBody.value == false) {
-      document.querySelector('.save-button').style.opacity = '0.5';
-      document.querySelector('.save-button').style.cursor = 'none';
-    }
+  ideaTitle.value = '';
+  ideaBody.value = '';
   }
 }
 
@@ -66,82 +61,71 @@ function deleteIdeaCard(event) {
       }
     }
   }
-  regenerateIdeaCards();
-}
-
-function regenerateIdeaCards() {
-  ideaCardGrid.innerHTML = '';
-  for (var i = 0; i < ideas.length; i++) {
-    if (ideas[i].star === false) {
-    ideaCardGrid.innerHTML += `
-    <div class="idea-card">
-      <div class="user-idea-header">
-        <img class="star" id=${ideas[i].id} src="./assets/star.svg" alt="star"/>
-        <img id=${ideas[i].id} class="delete-icon" src="./assets/delete.svg" alt="delete-icon"/>
-        <img class="delete-active hidden" src="./assets/delete-active.svg" alt="delete-icon-active"/>
-      </div>
-      <div class="user-idea">
-        <h3>${ideas[i].title}</h3>
-        <p>${ideas[i].body}</p>
-      </div>
-      <div class="user-idea-footer">
-        <img src="./assets/comment.svg" alt="comment-icon"/>
-        <section class="comment-button">Comment</section>
-        <label class="comment hidden"></label>
-      </div>
-    </div>
-    `
-  } else if (ideas[i].star === true) {
-    ideaCardGrid.innerHTML += `
-    <div class="idea-card">
-      <div class="user-idea-header">
-        <img class="star-active" id=${ideas[i].id} src="./assets/star-active.svg" alt="star-active"/>
-        <img id=${ideas[i].id} class="delete-icon" src="./assets/delete.svg" alt="delete-icon"/>
-        <img class="delete-active hidden" src="./assets/delete-active.svg" alt="delete-icon-active"/>
-      </div>
-      <div class="user-idea">
-        <h3>${ideas[i].title}</h3>
-        <p>${ideas[i].body}</p>
-      </div>
-      <div class="user-idea-footer">
-        <img src="./assets/comment.svg" alt="comment-icon"/>
-        <section class="comment-button">Comment</section>
-        <label class="comment hidden"></label>
-      </div>
-    </div>
-    `
-    }
+  if (searchIdeasStatus === false) {
+    regenerateIdeaCards(ideas);
+  } else {
+    regenerateIdeaCards(searchIndex);
   }
 }
 
-function changeImage(event) {
-  event.preventDefault();
-  if (event.target.className === 'star' || event.target.className === 'star-active') {
-    for (var i = 0; i < ideas.length; i++) {
-      if (ideas[i].id == event.target.id) {
-        ideas[i].updateIdea();
-      }
-    }
-    regenerateIdeaCards();
-  }
-}
-
-function showStarredIdeas(event) {
-  event.preventDefault();
+function regenerateIdeaCards(functionInput) {
   ideaCardGrid.innerHTML = '';
-  if (event.target.className === 'show-starred-ideas') {
+  if (filterIdeasStatus === false) {
     for (var i = 0; i < ideas.length; i++) {
-      if (ideas[i].star === true) {
+      if (functionInput[i].star === false) {
         ideaCardGrid.innerHTML += `
         <div class="idea-card">
           <div class="user-idea-header">
-            <img class="star-active" id=${ideas[i].id} src="./assets/star-active.svg" alt="star-active"/>
-            <img id=${ideas[i].id} class="delete-icon" src="./assets/delete.svg" alt="delete-icon"/>
+            <img class="star" id=${functionInput[i].id} src="./assets/star.svg" alt="star"/>
+            <img id=${functionInput[i].id} class="delete-icon" src="./assets/delete.svg" alt="delete-icon"/>
             <img class="delete-active hidden" src="./assets/delete-active.svg" alt="delete-icon-active"/>
           </div>
           <div class="user-idea">
-            <h3>${ideas[i].title}</h3>
-            <p>${ideas[i].body}</p>
+            <h3>${functionInput[i].title}</h3>
+            <p>${functionInput[i].body}</p>
+          </div>
+          <div class="user-idea-footer">
+            <img src="./assets/comment.svg" alt="comment-icon"/>
+            <section class="comment-button">Comment</section>
+            <label class="comment hidden"></label>
+          </div>
+        </div>
+        `
+    } else if (functionInput[i].star === true) {
+      ideaCardGrid.innerHTML += `
+      <div class="idea-card">
+        <div class="user-idea-header">
+          <img class="star-active" id=${functionInput[i].id} src="./assets/star-active.svg" alt="star-active"/>
+          <img id=${functionInput[i].id} class="delete-icon" src="./assets/delete.svg" alt="delete-icon"/>
+          <img class="delete-active hidden" src="./assets/delete-active.svg" alt="delete-icon-active"/>
+        </div>
+        <div class="user-idea">
+          <h3>${functionInput[i].title}</h3>
+          <p>${functionInput[i].body}</p>
+        </div>
+        <div class="user-idea-footer">
+          <img src="./assets/comment.svg" alt="comment-icon"/>
+          <section class="comment-button">Comment</section>
+          <label class="comment hidden"></label>
+        </div>
+      </div>
+      `
+      }
+    }
+  }
+  if (filterIdeasStatus === true) {
+    for (var i = 0; i < ideas.length; i++) {
+      if (functionInput[i].star === true) {
+        ideaCardGrid.innerHTML += `
+        <div class="idea-card">
+          <div class="user-idea-header">
+            <img class="star" id=${functionInput[i].id} src="./assets/star.svg" alt="star"/>
+            <img id=${functionInput[i].id} class="delete-icon" src="./assets/delete.svg" alt="delete-icon"/>
+            <img class="delete-active hidden" src="./assets/delete-active.svg" alt="delete-icon-active"/>
+          </div>
+          <div class="user-idea">
+            <h3>${functionInput[i].title}</h3>
+            <p>${functionInput[i].body}</p>
           </div>
           <div class="user-idea-footer">
             <img src="./assets/comment.svg" alt="comment-icon"/>
@@ -155,96 +139,171 @@ function showStarredIdeas(event) {
   }
 }
 
-function showAllIdeas() {
+function changeImage(event) {
+  event.preventDefault();
+  if (event.target.className === 'star' || event.target.className === 'star-active') {
+    for (var i = 0; i < ideas.length; i++) {
+      if (ideas[i].id == event.target.id) {
+        ideas[i].updateIdea();
+      }
+    }
+  }
+  if (searchIdeasStatus === false) {
+    regenerateIdeaCards(ideas);
+  } else {
+    regenerateIdeaCards(searchIndex);
+  }
+}
+
+function showStarredIdeas(event) {
+  event.preventDefault();
+  if (event.target.className === 'show-starred-ideas') {
+    toggleFilterButton();
+    if (searchIdeasStatus === false) {
+      regenerateIdeaCards(ideas);
+    } else {
+      regenerateIdeaCards(searchIndex);
+    }
+  }
+}
+
+function showAllIdeas(event) {
+  event.preventDefault();
   if (event.target.className === 'show-all-ideas') {
-    regenerateIdeaCards();
+    toggleFilterButton();
+    if (searchIdeasStatus === false) {
+      regenerateIdeaCards(ideas);
+    } else {
+      regenerateIdeaCards(searchIndex);
+    }
   }
 }
 
 function toggleFilterButton() {
   event.preventDefault();
   borderBox.innerHTML = '';
-  if (filterIdeas === true) {
+  if (filterIdeasStatus === false) {
     borderBox.innerHTML += `
       <h4 class="starred-ideas">Filter Starred Ideas</h4>
       <button class="show-all-ideas">Show All Ideas</button>
       `
-    filterIdeas = false;
+    filterIdeasStatus = true;
   } else {
     borderBox.innerHTML += `
       <h4 class="starred-ideas">Filter Starred Ideas</h4>
       <button class="show-starred-ideas">Show Starred Ideas</button>
       `
-    filterIdeas = true;
+    filterIdeasStatus = false;
   }
 }
 
-function searchInputData() {
-  var inputString = searchBox.value;
-  // var inputNoPunctuation = inputString.replace(/[.,\/#!*;:{}$%\^&\=\-_`~()]/g,'');
-  // var inputData = inputNoPunctuation.split(' ');
-  var inputData = inputString.split(' ');
-  var lengthCheck = 0;
-  for (var i = 0; i < ideas.length; i++) {
-    for (var j = 0; j < inputData.length; j++) {
-      if (ideas[i].body.includes(inputData[j]) || ideas[i].title.includes(inputData[j])) {
-        lengthCheck++;
-      }
-    }
-    if (lengthCheck === inputData.length) {
-      searchIndex.push(ideas[i]);
-      lengthCheck = 0;
-    }
-  }
-}
+// function searchInputData() {
+//   var inputString = searchBox.value;
+//   // var inputNoPunctuation = inputString.replace(/[.,\/#!*;:{}$%\^&\=\-_`~()]/g,'');
+//   // var inputData = inputNoPunctuation.split(' ');
+//   var inputData = inputString.split(' ');
+//   var lengthCheck = 0;
+//   for (var i = 0; i < ideas.length; i++) {
+//     for (var j = 0; j < inputData.length; j++) {
+//       if (ideas[i].body.includes(inputData[j]) || ideas[i].title.includes(inputData[j])) {
+//         lengthCheck++;
+//       }
+//     }
+//     if (lengthCheck === inputData.length) {
+//       searchIndex.push(ideas[i]);
+//       lengthCheck = 0;
+//     }
+//   }
+// }
 
-function outputSearch() {
-  if (searchBox.value = '') {
-    regenerateIdeaCards();
-  } else {
-    searchInputData();
-    ideaCardGrid.innerHTML = '';
-    for (var i = 0; i < searchIndex.length; i++) {
-      if (searchIndex[i].star === false) {
-      ideaCardGrid.innerHTML += `
-      <div class="idea-card">
-        <div class="user-idea-header">
-          <img class="star" id=${searchIndex[i].id} src="./assets/star.svg" alt="star"/>
-          <img id=${searchIndex[i].id} class="delete-icon" src="./assets/delete.svg" alt="delete-icon"/>
-          <img class="delete-active hidden" src="./assets/delete-active.svg" alt="delete-icon-active"/>
-        </div>
-        <div class="user-idea">
-          <h3>${searchIndex[i].title}</h3>
-          <p>${searchIndex[i].body}</p>
-        </div>
-        <div class="user-idea-footer">
-          <img src="./assets/comment.svg" alt="comment-icon"/>
-          <section class="comment-button">Comment</section>
-          <label class="comment hidden"></label>
-        </div>
-      </div>
-      `
-    } else if (searchIndex[i].star === true) {
-      ideaCardGrid.innerHTML += `
-      <div class="idea-card">
-        <div class="user-idea-header">
-          <img class="star-active" id=${searchIndex[i].id} src="./assets/star-active.svg" alt="star-active"/>
-          <img id=${searchIndex[i].id} class="delete-icon" src="./assets/delete.svg" alt="delete-icon"/>
-          <img class="delete-active hidden" src="./assets/delete-active.svg" alt="delete-icon-active"/>
-        </div>
-        <div class="user-idea">
-          <h3>${searchIndex[i].title}</h3>
-          <p>${searchIndex[i].body}</p>
-        </div>
-        <div class="user-idea-footer">
-          <img src="./assets/comment.svg" alt="comment-icon"/>
-          <section class="comment-button">Comment</section>
-          <label class="comment hidden"></label>
-        </div>
-      </div>
-      `
-      }
-    }
-    searchIndex = [];
-  }
-}
+// function outputSearch() {
+//   if (searchBox.value = "") {
+//     for (var i = 0; i < ideas.length; i++) {
+//       if (ideas[i].star === true) {
+//         ideaCardGrid.innerHTML += `
+//         <div class="idea-card">
+//           <div class="user-idea-header">
+//             <img class="star-active" id=${ideas[i].id} src="./assets/star-active.svg" alt="star-active"/>
+//             <img id=${ideas[i].id} class="delete-icon" src="./assets/delete.svg" alt="delete-icon"/>
+//             <img class="delete-active hidden" src="./assets/delete-active.svg" alt="delete-icon-active"/>
+//           </div>
+//           <div class="user-idea">
+//             <h3>${ideas[i].title}</h3>
+//             <p>${ideas[i].body}</p>
+//           </div>
+//           <div class="user-idea-footer">
+//             <img src="./assets/comment.svg" alt="comment-icon"/>
+//             <section class="comment-button">Comment</section>
+//             <label class="comment hidden"></label>
+//           </div>
+//         </div>
+//         `
+//       }
+//     }
+//       for (var i = 0; i < ideas.length; i++) {
+//         if (ideas[i].star === true) {
+//           ideaCardGrid.innerHTML += `
+//           <div class="idea-card">
+//             <div class="user-idea-header">
+//               <img class="star-active" id=${ideas[i].id} src="./assets/star-active.svg" alt="star-active"/>
+//               <img id=${ideas[i].id} class="delete-icon" src="./assets/delete.svg" alt="delete-icon"/>
+//               <img class="delete-active hidden" src="./assets/delete-active.svg" alt="delete-icon-active"/>
+//             </div>
+//             <div class="user-idea">
+//               <h3>${ideas[i].title}</h3>
+//               <p>${ideas[i].body}</p>
+//             </div>
+//             <div class="user-idea-footer">
+//               <img src="./assets/comment.svg" alt="comment-icon"/>
+//               <section class="comment-button">Comment</section>
+//               <label class="comment hidden"></label>
+//             </div>
+//           </div>
+//           `}
+//         }
+//
+//   searchInputData();
+//   ideaCardGrid.innerHTML = '';
+//   for (var i = 0; i < searchIndex.length; i++) {
+//     if (searchIndex[i].star === false) {
+//     ideaCardGrid.innerHTML += `
+//     <div class="idea-card">
+//       <div class="user-idea-header">
+//         <img class="star" id=${searchIndex[i].id} src="./assets/star.svg" alt="star"/>
+//         <img id=${searchIndex[i].id} class="delete-icon" src="./assets/delete.svg" alt="delete-icon"/>
+//         <img class="delete-active hidden" src="./assets/delete-active.svg" alt="delete-icon-active"/>
+//       </div>
+//       <div class="user-idea">
+//         <h3>${searchIndex[i].title}</h3>
+//         <p>${searchIndex[i].body}</p>
+//       </div>
+//       <div class="user-idea-footer">
+//         <img src="./assets/comment.svg" alt="comment-icon"/>
+//         <section class="comment-button">Comment</section>
+//         <label class="comment hidden"></label>
+//       </div>
+//     </div>
+//     `
+//   } else if (searchIndex[i].star === true) {
+//     ideaCardGrid.innerHTML += `
+//     <div class="idea-card">
+//       <div class="user-idea-header">
+//         <img class="star-active" id=${searchIndex[i].id} src="./assets/star-active.svg" alt="star-active"/>
+//         <img id=${searchIndex[i].id} class="delete-icon" src="./assets/delete.svg" alt="delete-icon"/>
+//         <img class="delete-active hidden" src="./assets/delete-active.svg" alt="delete-icon-active"/>
+//       </div>
+//       <div class="user-idea">
+//         <h3>${searchIndex[i].title}</h3>
+//         <p>${searchIndex[i].body}</p>
+//       </div>
+//       <div class="user-idea-footer">
+//         <img src="./assets/comment.svg" alt="comment-icon"/>
+//         <section class="comment-button">Comment</section>
+//         <label class="comment hidden"></label>
+//       </div>
+//     </div>
+//     `
+//     }
+//   }
+//   searchIndex = [];
+// }
